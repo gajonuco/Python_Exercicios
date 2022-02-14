b5b0c43d3409d98cdb59575203c6726334cf1121
gajonuco <gajonuco@gmail.com>
2022-02-14 17:25:47 -0300

    Exercício em Python
    Exemplo de um exercício em Python

---
+++ Python/exercio001.py
@@ -0,0 +1,20 @@
+import statistics
+alum_aprov = not_aprov = notas = []
+alum_repro = not_repro = []
+for i in range(1,5):
+  print(f'Digite as notas do {i}º alumno: \n')
+  for j in range(1,5):
+    notas.append(float(input(f'{j}º Nota: ')))
+  media = statistics.mean(notas)
+  alumnos.append(notas[:])
+  if media >= 7.0:
+    alum_aprov.append(i)
+    not_aprov.append(media)
+    notas.clear()
+  else:
+    alum_repro.append(i)
+    not_repro.append(media)
+    notas.clear()
+
+print(f'Alumnos aprovados:\n {not_aprov}')
+print(f'\nAlumnos reprovados:\n {not_repro}')
