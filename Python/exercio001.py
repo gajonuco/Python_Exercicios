8f47adcc46f2eb6a51e3acc93578db47ee2423ef
gajonuco <gajonuco@gmail.com>
2022-02-15 16:47:47 -0300

    Python 001
    Incluindo o primer exercício de Python, e descartando o anterior.

--- Python/exercio001.py
+++ Python/exercio001.py
@@ -1,22 +1,5 @@
-import statistics
-alum_aprov = not_aprov = notas = []
-alum_repro = not_repro = []
-for i in range(1,5):
-  print(f'Digite as notas do {i}º alumno: \n')
-  for j in range(1,5):
-    notas.append(float(input(f'{j}º Nota: ')))
-  media = statistics.mean(notas)
-  alumnos.append(notas[:])
-  if media >= 7.0:
-    alum_aprov.append(i)
-    not_aprov.append(media)
-    notas.clear()
-  else:
-    alum_repro.append(i)
-    not_repro.append(media)
-    notas.clear()
-
-print(f'Alumnos aprovados:\n {not_aprov}')
-print(f'\nAlumnos reprovados:\n {not_repro}')
-
-#Adicione um comentário aqui no código para utilizar os comandos no teclado.
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+suma = num1
+suma += num2
+print('\n A suma deles é', suma)
