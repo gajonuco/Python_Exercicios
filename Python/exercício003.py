4473e31dac1f8450618d960d162d271af49ba4aa
gajonuco <gajonuco@gmail.com>
2022-02-15 17:16:55 -0300

    Update exercício003.py
    Resolved some mistakes.

--- Python/exercício003.py
+++ Python/exercício003.py
@@ -1,8 +1,8 @@
 cont = 0
 while True:
-cont = cont + 1
-cont(cont%2 == 0):
-   continue
-if(cont>9):
-   break
-print(cont, end = ';')
+    cont = cont + 1
+    if cont%2 == 0:
+        continue
+    if cont>9:
+        break
+    print(cont, end = ';')
