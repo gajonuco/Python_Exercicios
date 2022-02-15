fc34287e0700b11936445de3e9714632695ab3ca
gajonuco <gajonuco@gmail.com>
2022-02-15 17:01:29 -0300

    Create exercício002.py

---
+++ Python/exercício002.py
@@ -0,0 +1,6 @@
+from math import ceil,floor, sqrt
+num = int(input('Digite um numero: '))
+raiz = sqrt(num)
+print(f'\nA raíz de {num} é igual a: {raiz:.2f}')
+
+print(f'\n ==Seu inteiro mais baixo sería: {floor(raiz)} ==')
