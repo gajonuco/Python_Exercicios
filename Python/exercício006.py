30e5cfeefa1be2286549f3b291c5b896111ab244
gajonuco <gajonuco@gmail.com>
2022-02-20 12:38:52 -0300

    Create exercício006.py
    I need your help to resolve this code.

---
+++ Python/exercício006.py
@@ -0,0 +1,9 @@
+def suma(num, num2, num3):
+  return num + num2 + num3
+
+  num = input(int(f'Digite o 1º digito: '))
+  num2 = input(int(f'Digite o 2º digito: '))
+  num3 = input(int(f'Digite o 3º digito: '))
+
+resl = suma(num, num2, num3)
+print(f'\n{resl}')
