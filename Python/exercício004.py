d633d9463b59ee2b1164e1d465b12c72f8b9d154
gajonuco <gajonuco@gmail.com>
2022-02-20 11:31:52 -0300

    Create exercício004.py

---
+++ Python/exercício004.py
@@ -0,0 +1,4 @@
+dinhero = float(input('Quanto você ganha por hora?: '))
+mes = int(input('Digite o número de horas trabalhadas neste mês: '))
+salario = dinhero * mes
+print(f'O seu salário deste mês é: {salario}')
