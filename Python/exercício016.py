2ec9be9610ff98206160be4a1f21c56fd553a307
gajonuco <gajonuco@gmail.com>
2022-02-22 16:49:51 -0300

    Create exercício016.py

---
+++ Python/exercício016.py
@@ -0,0 +1,14 @@
+from numpy import mean
+alumno = list()
+notas = list()
+alu_aprov = []
+seq = 0
+for i in range(1,11):
+  alumno.append(int(input(print(f'\nDigite as notas do {i} aluno: \n')))
+  seq = i - 1
+  for j in range(1,5):
+    notas.append(int(append(print(f'{j}º NOTA: ')))
+  media = mean.(notas)
+  if media => 7.0:
+    alu_aprov.append(alumno[seq])
+print(f'Os alumnos aprovados são: \n{alu_aprov[:]}')
