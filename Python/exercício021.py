be702a0fcfae4891c75dc5740f1f7fef87b9cdf4
gajonuco <gajonuco@gmail.com>
2022-02-22 19:01:10 -0300

    Create exercício021.py

---
+++ Python/exercício021.py
@@ -0,0 +1,6 @@
+user = str(input('Digite o seu usuario: '))
+password  = str(input('Digite a sua contrasenha: '))
+while user == password:
+  password = str(input('Digite de novo sua contrasenha: '))
+print('Parabens!')
+print('Cadastro concluído')
