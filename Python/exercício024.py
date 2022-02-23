5f6e2f27f78f6e41ab5410597fcf399eb5671e7d
gajonuco <gajonuco@gmail.com>
2022-02-23 18:57:17 -0300

    Create exercício022.py

---
+++ Python/exercício024.py
@@ -0,0 +1,13 @@
+pop_a =  int(input('Digite a quant. da população "A": '))
+tax_a = float(input('Digite a sua taxa % de crescimento: '))
+pop_b = int(input('\nDigite a quant. da população "B": '))
+tax_b = float(input('Digite a sua taxa % de crescimento: '))
+cont = 0
+
+while pop_a < pop_b:
+  pop_a = pop_a + (pop_a * (tax_a / 100))
+  pop_b = pop_b + (pop_b * (tax_b / 100))
+  cont += 1
+
+print(f'\nLevo {cont} anos para que a Popu A superasse à Popu. B')
+print(f'Pop A: {round(pop_a)} e Pop B: {round(pop_a)}')
