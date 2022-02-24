d51a5d65afed2165ebf662ed61b087950b552549
gajonuco <gajonuco@gmail.com>
2022-02-24 16:48:42 -0300

    Update exercício07.py and Create exercício08.py

--- Exercícios_de_Repetiçao/exercício07.py
+++ Exercícios_de_Repetiçao/exercício07.py
@@ -1,7 +1,7 @@
-]#Faça um programa que leia 5 números e informe o maior número.
+#Faça um programa que leia 5 números e informe o maior número.
 
 num = list()
 for i in range(1,6):
   num.append(int(input(f'Introduzca o {i}º número: ')))
 
-print(f'O maior número é: {max(num)}')
+print(f'\nO maior número é: {max(num)}')
---
+++ Exercícios_de_Repetiçao/exercício08.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia 5 números e informe a soma e a média dos números.
+
+from statistics import mean
+num = list()
+for i in range(1,6):
+  num.append(int(input(f'Introduzca o {i}º número: ')))
+
+print(f'\nA suma dos números é: {sum(num)}')
+print(f'A media dos números é: {mean(num)}')
